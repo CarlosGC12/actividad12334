e46c5a1bab49dd695be5d79b9354936bdf800c01
CarlosGC12 <ca.galleguillos@duocuc.cl>
2024-05-07 21:01:29 +0000

    actividad haciendo

---
+++ sample-app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+echo "FROM ubuntu:latest" >> /Dockerfile
+echo "apt-get update && \
+        apt-get install -y apche2 && \
+        rm -rf /var/lib/apt/lists/*" >> /Dockerfile
+echo "COPY  index.html /var/www/html" >> /Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> /Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD ["usr/sbin/apache2ctl", "-D", "FOREGROUND"]" >> /Dockerfile
+
+docker build -t sampleactividad .
+docker run -t -d -p 8080:8080 --name samplerunning sampleactividad
+docker ps -a 
